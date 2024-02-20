c1b5ba568ebb46c5079b603ae413c31ab0c8c273
Chitac Ruxanda <123298151+Ruxanda-Maria@users.noreply.github.com>
2024-02-20 18:39:01 +0200

    build(creat new controlers and edit layout)

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,5 +13,14 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
