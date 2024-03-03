4eeae2805b915df61b6c34944413f8a2a0317124
Chitac Ruxanda <123298151+Ruxanda-Maria@users.noreply.github.com>
2024-03-03 12:40:12 +0200

    build

--- WebApplication1/App_Start/BundelConfig.cs
+++ WebApplication1/App_Start/BundelConfig.cs
@@ -19,6 +19,25 @@ namespace WebApplication1.App_Start
             // CSS-ul aplicației
             bundles.Add(new StyleBundle("~/bundles/app/css").Include("~/Templaid/css/icons.min.css", "~/Templaid/css/app.min.css")
                  .Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            //Vndor js
+            bundles.Add(new ScriptBundle("~/vendor/js").Include(
+                "~/Templaid/js/vendor.min.js"
+                ));
+
+            // App js
+            bundles.Add(new ScriptBundle("~/app/js").Include(
+               "~/Templaid/js/app.min.js"
+               ));
+
+            //Index js
+
+            bundles.Add(new ScriptBundle("~/index/js").Include(
+                 "~/Templaid/libs/jquery-knob/jquery.knob.min.js",
+                 "~/Templaid/libs/peity/jquery.peity.min.js",
+                 "~/ Templaid/libs/jquery-sparkline/jquery.sparkline.min.js",
+                 "~/Templaid/js/pages/dashboard-1.init.js"
+                 ));
         }
     }
 }
