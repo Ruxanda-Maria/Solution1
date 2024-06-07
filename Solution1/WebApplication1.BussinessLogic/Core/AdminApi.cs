7c23becd4e4a4bb98efdeb35dee88a4b14033016
Chitac <ruxandamariachitac@gmail.com>
2024-06-07 07:07:01 -0700

    Added function for Admin to add new users

--- Solution1/WebApplication1.BussinessLogic/Core/AdminApi.cs
+++ Solution1/WebApplication1.BussinessLogic/Core/AdminApi.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApplication1.BussinessLogic.DBModel;
+using WebApplication1.Domain.Entities.Admin;
+using WebApplication1.Domain.Entities.User;
+using WebApplication1.Helper;
 
 namespace WebApplication1.BussinessLogic.Core
 {
     public class AdminApi
     {
+        internal BoolResp AddUserAction(AddUserData data)
+        {
+            using (var db = new TableContext())
+            {
+                UDbTable existingUser = db.Users.FirstOrDefault(u => u.Username == data.Username);
+                if (existingUser != null)
+                {
+                    return new BoolResp { Status = false, StatusMsg = "User already registered." };
+                }
+
+                var newUser = new UDbTable
+                {
+                    Username = data.Username,
+                    Password = LoginHelper.HashGen(data.Password),
+                    Level = data.Level,
+                    LastLogin = DateTime.Now,
+                    LastIp = "None",
+                    Image = "default.png",
+                };
+                db.Users.Add(newUser);
+                db.SaveChanges();
+            }
+            return new BoolResp { Status = true };
+        }
     }
 }
--- Solution1/WebApplication1.Domain/Entities/Admin/AddUserData.cs
+++ Solution1/WebApplication1.Domain/Entities/Admin/AddUserData.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApplication1.Domain.Enums;
 
 namespace WebApplication1.Domain.Entities.Admin
 {
     public class AddUserData
     {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime LastLogin { get; set; }
+
+        public string LastIp { get; set; }
+
+        public URole Level { get; set; }
+
+        public string Image { get; set; }
     }
 }
