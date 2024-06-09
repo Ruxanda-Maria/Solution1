0641bd08b2ada6a43c23a2c25101633898e9bd3d
Adarincik <efros.adarina03@gmail.com>
2024-06-09 20:44:32 +0300

    final changes

--- Solution1/WebApplication1/Models/UserRegister.cs
+++ Solution1/WebApplication1/Models/UserRegister.cs
@@ -5,9 +5,12 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
-     public class UserRegister
-     {
-          public string Username { get; set; }
-          public string Password { get; set; }
-     }
+    public class UserRegister
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
 }
